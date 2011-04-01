72ed74a6101cd0b300789f3e1d8a11ce19e46abf
Max Nordlund <max.nordlund@gmail.com>
2011-04-01 14:44:19 +0200

    Added Main class

---
+++ src/se/kth/csc/umbra/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package se.kth.csc.umbra;
+
+/**
+ * @author max.nordlund
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
