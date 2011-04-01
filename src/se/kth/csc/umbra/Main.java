b919eca42ceb4819558da4d57eb2d5518c4e4386
Max Nordlund <max.nordlund@gmail.com>
2011-04-01 15:00:43 +0200

    Added check for args length.

--- src/se/kth/csc/umbra/Main.java
+++ src/se/kth/csc/umbra/Main.java
@@ -9,12 +9,17 @@ package se.kth.csc.umbra;
  */
 public final class Main {
 
+	private static final String USAGE = "Project-Umbra path";
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if(args.length < 1) {
+			System.out.println(USAGE);
+			System.exit(0);
+		}
 	}
 
 }
