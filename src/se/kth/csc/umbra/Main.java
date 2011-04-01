80a5427818a193f673418452ceb30f85e271f3e9
Max Nordlund <max.nordlund@gmail.com>
2011-04-01 16:04:50 +0200

    Added some comments.

--- src/se/kth/csc/umbra/Main.java
+++ src/se/kth/csc/umbra/Main.java
@@ -1,25 +1,34 @@
 /**
+ * Project Umbra is a Mhasiran text editor.
  * 
+ * It is a text editor for the fictional language Mhasira, which text is written
+ * in a special font in vertical columns from left to right.
+ * 
+ * The editor is capable of displaying text vertically and allows the user to
+ * write simple texts with relative ease. The editor displays all correct
+ * variations of the characters, something other programs such as word is
+ * incapable of.
  */
 package se.kth.csc.umbra;
 
 /**
- * @author max.nordlund
- *
+ * This class is responsible for setting up the object graph and starting the
+ * application.
+ * 
+ * @author Max Nordlund
+ * @version 2011.04.01
  */
 public final class Main {
-
 	private static final String USAGE = "Project-Umbra path";
 
 	/**
 	 * @param args
+	 *            Arguments from the command line.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if(args.length < 1) {
+		if (args.length < 1) {
 			System.out.println(USAGE);
 			System.exit(0);
 		}
 	}
-
 }
