22894a122374b3b4f9fc5f6cc179e69e9e16e5c6
Max Nordlund <max.nordlund@gmail.com>
2011-04-03 16:23:31 +0200

    Added some initial swing code. It now displays a menubar with three buttons, each having an icon and a tooltip. Below is a textarea.
    Also included a ant script for building a runnable jar.

--- src/se/kth/csc/umbra/Main.java
+++ src/se/kth/csc/umbra/Main.java
@@ -11,6 +11,18 @@
  */
 package se.kth.csc.umbra;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.*;
+
 /**
  * This class is responsible for setting up the object graph and starting the
  * application.
@@ -30,5 +42,106 @@ public final class Main {
 			System.out.println(USAGE);
 			System.exit(0);
 		}
+
+		JFrame frame = new JFrame("This is a test");
+		Container contentPane = frame.getContentPane();
+
+		JTextArea text = new JTextArea();
+		contentPane.add(text);
+
+		makeMenuBar(frame);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setBounds(50, 50, 300, 500);
+		frame.setVisible(true);
+	}
+
+	/**
+	 * @param frame
+	 */
+	private static void makeMenuBar(JFrame frame) {
+		JMenuBar menubar = new JMenuBar();
+		frame.setJMenuBar(menubar);
+
+		JButton newFile = makeButton("newFile", "[N]");
+		newFile.setToolTipText("Creates a new document.");
+		newFile.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menubar.add(newFile);
+
+		JButton open = makeButton("open", "[O]");
+		open.setToolTipText("Open a document.");
+		open.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menubar.add(open);
+
+		JButton save = makeButton("save", "[S]");
+		save.setToolTipText("Save the current document.");
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		menubar.add(save);
+	}
+
+	/**
+	 * Tries to read the contents from the file located at the supplied path.
+	 * Important <em>not</em> to prefix the path with any slash ('/'), e.g.
+	 * <code>"open.png"</code> and not <code>"/open.png"</code> or
+	 * <code>"./open.png"</code>.
+	 * 
+	 * @param path
+	 *            The relative path from this program or the absolute path from
+	 *            within this programs jar.
+	 * @return An icon from the supplid path.
+	 */
+	private static JButton makeButton(String path, String desc) {
+		Icon icon = null;
+		InputStream input = null;
+		try {
+			input = new FileInputStream("./" + path + ".png");
+			icon = readIcon(input);
+		} catch (FileNotFoundException e) {
+			input = Main.class.getResourceAsStream("/" + path + ".png");
+			try {
+				icon = readIcon(input);
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (input != null) {
+					input.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (icon == null) {
+			return new JButton(desc);
+		}
+		return new JButton(icon);
+	}
+
+	/**
+	 * @param input
+	 * @return
+	 * @throws IOException
+	 */
+	private static Icon readIcon(InputStream input) throws IOException {
+		Icon icon = null;
+		BufferedImage img = ImageIO.read(input);
+		icon = new ImageIcon(img);
+		return icon;
 	}
 }
